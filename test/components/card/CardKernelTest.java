7d8489bffd19b1cc54cdba735ef966be75c2246b
Allen Thomas <allenthom05@gmail.com>
2024-04-19 17:02:28 -0400

    Update CardKernelTest.java

--- test/components/card/CardKernelTest.java
+++ test/components/card/CardKernelTest.java
@@ -2,8 +2,7 @@ package components.card;
 
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
 
 import org.junit.Test;
 
